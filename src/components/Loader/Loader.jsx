44ad4399185f96ce89dec90052a7027d90e2b99e
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-10-11 16:30:53 +0200

    fetch

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,5 @@
+import { InfinitySpin } from 'react-loader-spinner';
+
+export default function Loader() {
+  return <InfinitySpin width="200" color="#e02070" />;
+}
--- src/services/api.js
+++ src/services/api.js
@@ -2,3 +2,34 @@ import axios from 'axios';
 
 const KEY = 'ef600613e6bfa5a468c80238f192e03f';
 const BASE_URL = 'https://api.themoviedb.org/3';
+
+export const getTrendingMovies = async () => {
+  const data = await axios.get(`${BASE_URL}trending/movie/day?api_key=${KEY}`);
+  return data;
+};
+
+export const getSearchMovies = async query => {
+  const data = await axios.get(
+    `${BASE_URL}trending/movie?api_key=${KEY}&query=${query}`
+  );
+  return data;
+};
+
+export const getMovieDetails = async movieID => {
+  const data = await axios.get(`${BASE_URL}trending/${movieID}?api_key=${KEY}`);
+  return data;
+};
+
+export const getMovieCasts = async movieID => {
+  const data = await axios.get(
+    `${BASE_URL}trending/${movieID}/credits?api_key=${KEY}`
+  );
+  return data;
+};
+
+export const getMovieReviews = async movieID => {
+  const data = await axios.get(
+    `${BASE_URL}trending/${movieID}/reviews?api_key=${KEY}`
+  );
+  return data;
+};
