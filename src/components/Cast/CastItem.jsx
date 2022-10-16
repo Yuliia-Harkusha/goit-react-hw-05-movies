82cf758ed2b8c249b1a6c954fa684c9abf2e036d
Yuliia-Harkusha <97845497+Yuliia-Harkusha@users.noreply.github.com>
2022-10-17 00:17:58 +0200

    add default cast  picture

--- src/components/Cast/CastItem.jsx
+++ src/components/Cast/CastItem.jsx
@@ -1,5 +1,7 @@
 import { nanoid } from 'nanoid';
 import { Image, Name } from './Cast.styled';
+// import img from '../image/notFoundImage.jpg';
+
 const IMG_URL = 'https://images.tmdb.org/t/p/w500';
 
 export const CastItem = ({ cast }) => {
@@ -9,7 +11,12 @@ export const CastItem = ({ cast }) => {
         {profile_path ? (
           <Image src={`${IMG_URL}${profile_path}`} alt={original_name} />
         ) : (
-          <img src="" alt={original_name} />
+          <img
+            src="https://sd.keepcalms.com/i-w600/meow-208.jpg"
+            alt={original_name}
+            width={250}
+            height={375}
+          />
         )}
         <Name>{original_name}</Name>
         <Name>Character: {character}</Name>
